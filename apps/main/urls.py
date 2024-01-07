ad198509c69dc57c878b431d8fd4ffaa3679363e
ulan <u.isazhan@mail.ru>
2024-01-07 18:05:36 +0600

    create_ad templates

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create_ad', views.create_ad, name='create_ad'),
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def create_ad(request):
+    return render(request, 'main/create_ad.html')
