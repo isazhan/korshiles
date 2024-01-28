a8be6b54e93d88b75354140c0e2e98db37c9cd9c
ulan <u.isazhan@mail.ru>
2024-01-28 21:28:30 +0600

    login system optimization

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('login/', views.login_user, name='login_user'),
-    #path('logout/', views.logout_user, name='logout_user'),
-    path('register/', views.register, name='register'),
+    path('signup/', views.signup_user, name='signup'),
+    path('login/', views.login_user, name='login'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,17 +1,30 @@
-from django.shortcuts import render
-from django.contrib.auth import get_user_model
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import CustomUserCreationForm  # Create a form for user creation
+from django.contrib import messages
 
-# Create your views here.
-def register(request):
-    print('func')
-    create_user()
-    return render(request, 'users/register.html')
 
-def create_user():
-    User = get_user_model()
-    user = User.objects.create_user(
-        username='ulan', 
-        password='ulan', 
-        first_name='fn',
-        last_name='ln')
-    user.save()
+def signup_user(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully. Please log in.')
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'users/signup.html', {'form': form})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful.')
+            return redirect('/')  # Change 'home' to your desired home page
+        else:
+            messages.error(request, 'Invalid login credentials.')
+    return render(request, 'users/login.html')
