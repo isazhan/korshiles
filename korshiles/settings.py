c3fc7305301da6e9157b713118cc7fd475ac794c
ulan <u.isazhan@mail.ru>
2024-07-09 23:45:43 +0500

    logo fixes

--- korshiles/settings.py
+++ korshiles/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n=uxz%k%^j+s70mr#fuu!8e1lw1z(t11ounae*nq23cbk!h!f3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
