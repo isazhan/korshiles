4441eff63a57e340b99d11b89875f69ce8f3884a
ulan <u.isazhan@mail.ru>
2024-11-28 23:56:04 +0500

    add filter view

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('index', views.index, name='index'),
     path('ad', views.ad, name='ad'),
+    path('filter', views.filter, name='filter'),
 ]
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from db import get_db_handle as db
-
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     col = db()['ads']
@@ -14,5 +14,20 @@ def index(request):
     return JsonResponse(data, safe=False)
 
 
+@csrf_exempt
 def ad(request):
-    print(request)
+    ad = int(request.POST['ad'])
+    col = db()['ads']
+    doc = col.find_one({'ad': ad})
+    col.update_one({'ad': ad}, {'$set': {'views': doc['views']+1}})
+
+    doc['_id'] = str(doc['_id'])
+    
+    return JsonResponse(doc, safe=False)
+
+
+@csrf_exempt
+def filter(request):
+    print(request.POST)
+    print('Filter request')
+    return JsonResponse('good', safe=False)
