96409a0f5024b59e137f2333b0b8635461e65c4e
ulan <u.isazhan@mail.ru>
2025-01-02 01:10:57 +0500

    edit api index view

--- apps/api/views.py
+++ apps/api/views.py
@@ -3,8 +3,31 @@ from db import get_db_handle as db
 from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
+    #print('Index request: ', request.GET)
+
+    data = request.GET.dict()
+    #print(data)
+    filter_dict = {}
+    for key, value in data.items():
+        if key.endswith('_min') or key.endswith('_max'):
+            field, range_type = key.rsplit('_', 1)
+            if field not in filter_dict:
+                filter_dict[field] = {}
+            if not value == '':
+                if range_type == 'min':
+                    filter_dict[field]['$gte'] = int(value)
+                elif range_type == 'max':
+                    filter_dict[field]['$lte'] = int(value)
+        elif key=='city' or key=='district' or key=='type':
+            if not value == '':
+                filter_dict[key+'.id'] = value
+        else:
+            if not value == '':
+                filter_dict[key] = value
+    #print('Filter')
+    #print(filter_dict)
     col = db()['ads']
-    doc = col.find()
+    doc = col.find(filter_dict).sort('create_time', -1)
 
     data = []
     for item in doc:
