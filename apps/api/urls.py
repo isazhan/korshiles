ebac0a2f048faaf2b819a85ff5efa60e55efda15
ulan <u.isazhan@mail.ru>
2025-05-25 01:12:18 +0500

    urls edit

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('index', views.index, name='index'),
     path('ad', views.ad, name='ad'),
-    path('create_ad', views.CreateAdAPIView.as_view(), name='create_ad'),
+    path('api_create_ad', views.CreateAdAPIView.as_view(), name='api_create_ad'),
     path('api_login', views.LoginAPIView.as_view(), name='api_login'),
     path('api_logout', views.LogoutAPIView.as_view(), name='api_logout'),
     path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
