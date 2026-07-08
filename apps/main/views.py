56347e6b3c1d9918ebf22752d96454c6670a2620
ulan <u.isazhan@mail.ru>
2026-07-08 22:47:34 +0500

    add 404 page

--- apps/main/views.py
+++ apps/main/views.py
@@ -52,14 +52,18 @@ def search(request):
 def ad(request, ad):
     col = db()['ads']
     doc = col.find_one({'ad': ad})
-    if doc['publish'] == False and request.user.is_staff == False:
-        return redirect(index)
+    if doc:
+        if doc['publish'] == False and request.user.is_staff == False:
+            return redirect(index)
+        else:
+            col.update_one({'ad': ad}, {'$set': {'views': doc['views']+1}})
+            context = {'doc': doc}
+            template = loader.get_template('main/ad.html')
+            return HttpResponse(template.render(context, request))
     else:
-        col.update_one({'ad': ad}, {'$set': {'views': doc['views']+1}})
-        context = {'doc': doc}
-        template = loader.get_template('main/ad.html')
-        return HttpResponse(template.render(context, request))
-    
+        template = loader.get_template('main/404.html')
+        return HttpResponse(template.render({}, request), status=404)
+
 
 def robots_txt(request):
     lines = [
@@ -79,6 +83,11 @@ def policy(request):
 def terms(request):
     template = loader.get_template('main/terms.html')
     return HttpResponse(template.render({}, request))
+
+
+def page_not_found(request, exception):
+    template = loader.get_template('main/404.html')
+    return HttpResponse(template.render({}, request), status=404)
 
 
 def app_ads_txt(request):
